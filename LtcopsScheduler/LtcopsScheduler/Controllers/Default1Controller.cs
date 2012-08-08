c0dac1b8fe73a594de67f4be48d935ee0dd3eb0a
seandunford <sdunford@mail.usf.edu>
2012-08-07 23:18:05 -0400

    removed all bin and objects did some testing any and all experimenting with this project should use day 3 of http://www.codeproject.com/Articles/375182/Learn-MVC-Model-view-controller-Step-by-Step-in-7d#Day2:-Unittest,routingandoutboundURLS
    or other RAZOR tuts classic aspx engine wont work.

--- LtcopsScheduler/LtcopsScheduler/Controllers/Default1Controller.cs
+++ LtcopsScheduler/LtcopsScheduler/Controllers/Default1Controller.cs
@@ -6,18 +6,15 @@ using System.Web.Mvc;
 
 namespace LtcopsScheduler.Controllers
 {
-    public class HomeController : Controller
+    public class Default1Controller : Controller
     {
+        //
+        // GET: /Default1/
+
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
-            ViewData["CurrentTime"] = DateTime.Now.ToString();
             return View();
         }
 
-        public ActionResult About()
-        {
-            return View();
-        }
     }
 }
